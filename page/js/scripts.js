6f78d2ae2984ade64c3310c0ba437654c4e63d85
waqar ahmad <waqargpgc@gmail.com>
2017-05-20 19:12:56 -0700

    Add files via upload

---
+++ page/js/scripts.js
@@ -0,0 +1,14 @@
+(function($) {
+
+    'use strict';
+
+    $(document).ready(function() {
+        // Initializes search overlay plugin.
+        // Replace onSearchSubmit() and onKeyEnter() with 
+        // your logic to perform a search and display results
+        $(".list-view-wrapper").scrollbar();
+
+
+    });
+
+})(window.jQuery);
